5432219f052e62ddf00697d4170a7791c9ee1a39
Marcel Weber <mrweber@gmx.ch>
2022-08-31 10:34:48 +0200

    core logic finalized for event and random

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
-import {getTargetPosition} from "./coreLogic"
+import { useState, useEffect } from 'react'
+import {getTargetPosition, calculateHit, setRandomTargetPosition} from "./coreLogic"
 
 import './App.css'
 
 function App() {
+
+  const [currentHit, setCurrentHit] = useState([])
+
+  useEffect(() => {
+    const hitValue = calculateHit(currentHit)
+    console.log(hitValue)
+  }, [currentHit])
+
   return (
     <div id="mainContainer">
       <div id="targetContainer" onClick={(e) =>{
-        getTargetPosition(e, e.target)
+        setCurrentHit(getTargetPosition(e, e.target))
       }}>
       </div>
+      <button id="randomHit" onClick={() =>{
+        const randomHitPosition = setRandomTargetPosition()
+        setCurrentHit(randomHitPosition)
+      }}>Zufallstreffer</button>
     </div>
   )
 }
--- src/coreLogic.js
+++ src/coreLogic.js
@@ -2,8 +2,21 @@ export function getTargetPosition(e, element){
     const width = element.getBoundingClientRect().width
     const height =  element.getBoundingClientRect().height
     const sizeConstant = width/100
+    const xCoordinate = (e.nativeEvent.offsetX/sizeConstant)*2
+    const yCoordinate = (e.nativeEvent.offsetY/sizeConstant)*2
+    return [xCoordinate, yCoordinate]
+}
 
-    const distToCenter = Math.floor(Math.sqrt((Math.pow((((e.nativeEvent.offsetX/sizeConstant)*2)-100),2)+Math.pow((((e.nativeEvent.offsetY/sizeConstant)*2)-100),2))))
+export function setRandomTargetPosition(){
+    const randomX = Math.floor(Math.random()*201) 
+    const randomY = Math.floor(Math.random()*201) 
+    return [randomX, randomY]
+}
+
+export function calculateHit(hitPosition){
+    const x = hitPosition[0]
+    const y = hitPosition[1]
+    const distToCenter = Math.floor(Math.sqrt((Math.pow((x-100),2)+Math.pow((y-100),2))))
 	const hit = 100-distToCenter
-    console.log(hit)
+    return hit
 }

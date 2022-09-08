d3b9dbae035fcfe4700cd2498d6cf0dbf2347f44
Marcel Weber <mrweber@gmx.ch>
2022-09-08 12:03:43 +0200

    Added function to determine pixel coordinates of random shot

--- src/coreLogic.js
+++ src/coreLogic.js
@@ -7,6 +7,16 @@ export function getTargetPosition(e, element){
     return [xCoordinate, yCoordinate]
 }
 
+export function setTargetPosition(coordinates){
+    const element = document.getElementById("targetContainer")
+    const width = element.getBoundingClientRect().width
+    const height =  element.getBoundingClientRect().height
+    const sizeConstant = width/100
+    const xCoordinate = (coordinates[0]/2)*sizeConstant
+    const yCoordinate = (coordinates[1]/2)*sizeConstant
+    return [xCoordinate, yCoordinate]
+}
+
 export function setRandomTargetPosition(){
     const randomX = Math.floor(Math.random()*201) 
     const randomY = Math.floor(Math.random()*201) 
